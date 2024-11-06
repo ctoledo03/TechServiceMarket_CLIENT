7585ac9f4fb3f2d5d6df4078250049d7c44daa4e
Carlo Toledo <ctoledo2@my.centennialcollege.ca>
2024-11-05 21:48:05 -0500

    added hosting stuff

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,33 +1,15 @@
-// client/src/components/Services.js
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Services = () => {
-    const [services, setServices] = useState([]);
-
-    useEffect(() => {
-        axios.get('http://localhost:5000/api/services')
-            .then((response) => {
-                setServices(response.data);
-            })
-            .catch((error) => {
-                console.error('There was an error fetching the services!', error);
-            });
-    }, []);
-
-    return (
-        <div>
-            <h1>Tech Services</h1>
-            <ul>
-                {services.map(service => (
-                    <li key={service.id}>
-                        <h3>{service.name}</h3>
-                        <p>{service.description}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+const fetchServices = async () => {
+  try {
+    // Use the backend URL deployed on Render
+    const response = await axios.get('https://techservicemarket-backend.onrender.com/api/services');
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching services:", error);
+  }
 };
 
-export default Services;
+useEffect(() => {
+  fetchServices();
+}, []);
